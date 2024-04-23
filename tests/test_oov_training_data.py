1aa97b4886f8d911bff8b0a0a91c3ea2a302b05a
Sang Dao <daosysang@gmail.com>
2024-04-23 23:12:09 +0700

    Add test_filter_data_by_targeted_oov

--- tests/test_oov_training_data.py
+++ tests/test_oov_training_data.py
@@ -1,6 +1,11 @@
-from nomoroov.oov_training_data import cook_training_data, InMemoryDataCollector
-
 import pytest
+
+from nomoroov.oov_training_data import (
+    cook_training_data,
+    InMemoryDataCollector,
+    filter_data_by_targeted_oov
+)
+from nomoroov.base_nlp import Doc, Token
 
 
 test_parameters = [
@@ -159,3 +164,83 @@ def test_cook_training_data(list_of_item, window_size, expected):
     cook_training_data(list_of_item, data_collector=data_collector, window_size=window_size)
 
     assert expected == set(data_collector.data)
+
+
+def test_filter_data_by_targeted_oov():
+    sequences = [
+        Doc(
+            text='unit test unit test unit test',
+            tokens=[
+                Token(text='unit', is_oov=False),
+                Token(text='test', is_oov=False),
+                Token(text='unit', is_oov=False),
+                Token(text='test', is_oov=False),
+                Token(text='unit', is_oov=False),
+                Token(text='test', is_oov=False),
+            ],
+        ),
+        Doc(
+            text='hello world, how are you',
+            tokens=[
+                Token(text='hello', is_oov=False),
+                Token(text='world', is_oov=False),
+                Token(text=',', is_oov=False),
+                Token(text='how', is_oov=False),
+                Token(text='are', is_oov=False),
+                Token(text='you', is_oov=False),
+            ],
+        ),
+        Doc(
+            text='bonjour i am spacy how are you',
+            tokens=[
+                Token(text='bonjour', is_oov=True),
+                Token(text='i', is_oov=False),
+                Token(text='am', is_oov=False),
+                Token(text='spacy', is_oov=True),
+                Token(text='how', is_oov=False),
+                Token(text='are', is_oov=False),
+                Token(text='you', is_oov=False),
+            ],
+        ),
+        Doc(
+            text='I am spacy, how are you',
+            tokens=[
+                Token(text='i', is_oov=False),
+                Token(text='am', is_oov=False),
+                Token(text='spacy', is_oov=True),
+                Token(text=',', is_oov=False),
+                Token(text='how', is_oov=False),
+                Token(text='are', is_oov=False),
+                Token(text='you', is_oov=False),
+            ],
+        ),
+        Doc(
+            text='hello i am spacy',
+            tokens=[
+                Token(text='hello', is_oov=False),
+                Token(text='i', is_oov=False),
+                Token(text='am', is_oov=False),
+                Token(text='spacy', is_oov=False),
+            ],
+        ),
+        Doc(
+            text='bonjour, how are you today',
+            tokens=[
+                Token(text='bonjour', is_oov=True),
+                Token(text=',', is_oov=False),
+                Token(text='how', is_oov=False),
+                Token(text='are', is_oov=False),
+                Token(text='you', is_oov=False),
+                Token(text='today', is_oov=False),
+            ],
+        ),
+    ]
+
+    expected = ['I am spacy, how are you']
+
+    actual = filter_data_by_targeted_oov(target='spacy', docs=sequences)
+    actual = list(actual)
+
+    assert len(actual) == 1
+    assert expected == actual
+
